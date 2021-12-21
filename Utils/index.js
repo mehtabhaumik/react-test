409c33683330ab232bde26fc9bda995100e8de0d
Bhaumik Mehta <bhaumikmehta@Bhaumiks-MacBook-Air.local>
2021-12-21 18:35:21 -0500

    Adding PropTypes and tests

--- Utils/index.js
+++ Utils/index.js
@@ -1,4 +1,15 @@
+import checkPropTypes from "prop-types/checkPropTypes";
+
 export const getByTestAttr = (component, attr) => {
   const wrapper = component.find(`[data-test='${attr}']`);
   return wrapper;
+};
+
+export const checkProps = (component, expectedProps) => {
+  return checkPropTypes(
+    component.propTypes,
+    expectedProps,
+    "props",
+    component.name
+  );
 };
